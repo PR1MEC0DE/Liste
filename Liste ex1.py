c0069bb13a7bc6a4cc64fe89e10233ae1a4f0ce5
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-10-07 21:04:56 +0300

    Add files via upload

---
+++ Liste ex1.py
@@ -0,0 +1,37 @@
+prenume = ['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+varsta = [14, 23, 15, 14, 12, 41, 39]
+x = ("are varsta de")
+y = ("ani")
+y1 = ("de ani")
+print(prenume[0],x, varsta[0],y)
+print(prenume[1],x, varsta[1],y1)
+print(prenume[2],x, varsta[2],y)
+print(prenume[3],x, varsta[3],y)
+print(prenume[4],x, varsta[4],y)
+print(prenume[5],x, varsta[5],y1)
+print(prenume[6],x, varsta[6],y1)
+prenume.append("Andreea")
+varsta.append(34)
+prenume.append("Ioan")
+varsta.append(23)
+print("---------------------------------")
+print("---------------------------------")
+print("Exercitiul modificat")
+print("a)")
+print(" ")
+print(prenume[0],x, varsta[0],y)
+print(prenume[1],x, varsta[1],y1)
+print(prenume[2],x, varsta[2],y)
+print(prenume[3],x, varsta[3],y)
+print(prenume[4],x, varsta[4],y)
+print(prenume[5],x, varsta[5],y1)
+print(prenume[6],x, varsta[6],y1)
+print(prenume[7],x, varsta[7],y1)
+print(prenume[8],x, varsta[8],y1)
+
+
+
+
+
+
+
