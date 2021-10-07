597a89bf82ffa7e333f38488b96e19b231f8ad8a
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-10-07 19:49:58 +0300

    Update Liste ex1.py

--- Liste ex1.py
+++ Liste ex1.py
@@ -6,11 +6,4 @@ print(prenume[2],"are varsta de", varsta[2], "ani")
 print(prenume[3],"are varsta de", varsta[3], "ani")
 print(prenume[4],"are varsta de", varsta[4], "ani")
 print(prenume[5],"are varsta de", varsta[5], "de ani")
-
-
-
-
-
-
-
-
+print(prenume[6],"are varsta de", varsta[6], "de ani")
