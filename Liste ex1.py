768ff67b62b4b5a5d8d4cf2f3e039662f6412482
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-10-07 21:02:06 +0300

    Delete Liste ex1.py

--- Liste ex1.py
+++
@@ -1,9 +0,0 @@
-prenume = ['Mihai','George','Ana','Dan','Ion','Geta','Vio']
-varsta = [14, 23, 15, 14, 12, 41, 39]
-print(prenume[0],"are varsta de", varsta[0], "ani")
-print(prenume[1],"are varsta de", varsta[1], "de ani")
-print(prenume[2],"are varsta de", varsta[2], "ani")
-print(prenume[3],"are varsta de", varsta[3], "ani")
-print(prenume[4],"are varsta de", varsta[4], "ani")
-print(prenume[5],"are varsta de", varsta[5], "de ani")
-print(prenume[6],"are varsta de", varsta[6], "de ani")
